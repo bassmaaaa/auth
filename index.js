991ca8a81aaffe4f3822c46f43d7a42ca062c3f0
Bassma <bassmaarabu@gmail.com>
2026-01-01 21:17:07 +0200

    login api

--- index.js
+++ index.js
@@ -115,3 +115,63 @@ console.log("Email count:", result[0].count);
     });
   });
    });
+
+
+   app.post('/login', (req, res) => {{}
+    if (!req.body) {
+    return res.status(400).json({ error: "Request body is missing." });
+  }
+
+  if (!isValidJSON(JSON.stringify(req.body))) {
+    return res.status(400).json({ error: "Invalid JSON format." });
+  }
+
+   const {username, email, password } = req.body;
+  const errors = [];
+  if (!username || !email) {
+    errors.push("username or email are required.");
+  }
+  if (!password) {
+    errors.push("Password is required.");
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json({ errors: errors });
+  }
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+  }
+  if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 6 characters" });
+    }
+  if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ error: "Username must be at least 3 characters" });
+    }
+
+    const query = "SELECT * FROM users WHERE email = ? OR username = ?";
+    db.query(query, [email,username], async (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Database error" });
+      }
+      if (result.length === 0) {
+        return res.status(401).json({ error: "Invalid email or password" });//401 not 404 krml l security 
+      }
+     
+      const user = result[0];
+      const hashedpassword = await bcrypt.hash(password, 10);
+      console.log("hashedPassword from request:", hashedpassword);
+      console.log("Hashed password from DB:", user.hashedPassword);
+     const isPasswordValid = await bcrypt.compare(password, user.hashedPassword); 
+      console.log("Password is valid?", isPasswordValid);
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Invalid email or password" });
+      }
+      res.status(200).json({ message: "Login successful", user });
+    });
+  });
