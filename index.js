faf63c72bd54fc67d109c00b33260ce99cc263e2
Bassma <bassmaarabu@gmail.com>
2026-01-02 17:36:17 +0200

    deploy auth backend

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000")
-})
+//app.listen(5000,()=>{
+ //   console.log("Server is running on port 5000")
+//})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const db = mysql.createConnection({
   host:"localhost",
   user:"root",
